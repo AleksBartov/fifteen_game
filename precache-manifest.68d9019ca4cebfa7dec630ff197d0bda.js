764ad94c51ecd25806463d9a7bf53aebef985fa2
Aleksandr <aleksandrbartov@yandex.ru>
2020-02-28 01:15:04 +0300

    Updates

---
+++ precache-manifest.68d9019ca4cebfa7dec630ff197d0bda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455b3ee1465c97f500e70a02d2ee12d9",
+    "url": "/fifteen-game/index.html"
+  },
+  {
+    "revision": "66242d028a7e83380c12",
+    "url": "/fifteen-game/static/css/main.251bd3d3.chunk.css"
+  },
+  {
+    "revision": "b50e46802fde422c0707",
+    "url": "/fifteen-game/static/js/2.b8a1921d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/fifteen-game/static/js/2.b8a1921d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66242d028a7e83380c12",
+    "url": "/fifteen-game/static/js/main.82e5a083.chunk.js"
+  },
+  {
+    "revision": "fa4f709ea2039f0c3134",
+    "url": "/fifteen-game/static/js/runtime-main.9f385c5d.js"
+  }
+]);
--- src/constants/constants.js
+++
@@ -1,5 +0,0 @@
-export const CONSTANTS = {
-    NUM_OF_CARDS: 16,
-    CARD_WIDTH: 69,
-    PERIOD: 4,
-}
